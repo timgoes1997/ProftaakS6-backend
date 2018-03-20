199a99013a84097cd0af33915445f31014fbe03d
biepbot <biepbot@gmail.com>
2018-03-20 14:33:50 +0100

    CORS update

--- src/main/com/github/fontys/base/RESTCorsResponseFilter.java
+++ src/main/com/github/fontys/base/RESTCorsResponseFilter.java
@@ -13,8 +13,10 @@ public class RESTCorsResponseFilter implements ContainerResponseFilter {
 
     @Override
     public void filter( ContainerRequestContext requestCtx, ContainerResponseContext responseCtx ) throws IOException {
-        responseCtx.getHeaders().add( "Access-Control-Allow-Origin", "*" );
+        responseCtx.getHeaders().add( "Access-Control-Allow-Origin", "*" ); // OR: our domain
         responseCtx.getHeaders().add( "Access-Control-Allow-Credentials", "true" );
-        responseCtx.getHeaders().add( "Access-Control-Allow-Methods", "GET, POST, DELETE, PUT" );
+        responseCtx.getHeaders().add( "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD" );
+        responseCtx.getHeaders().add( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization" );
+        responseCtx.getHeaders().add( "Access-Control-Max-Age", "1209600" );
     }
 }
