6ac450521cb115980a10ea044cd54a80fba3d95d
Edwin van Rooij <edwinvrooij@gmail.com>
2018-04-18 14:06:56 +0200

    add integrationtest setup

--- integrationtesting/src/test/java/com/github/fontys/AppTest.java
+++ integrationtesting/src/test/java/com/github/fontys/AppTest.java
@@ -8,22 +8,18 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.jayway.restassured.RestAssured.urlEncodingEnabled;
-import static org.junit.Assert.*;
-
 public class AppTest {
 
     @BeforeClass
     public static void setup() {
-        RestAssured.baseURI = "http://localhost";
-        RestAssured.port = 8080;
-        RestAssured.basePath = "/kwetter/";
+//        RestAssured.baseURI = "http://localhost";
+//        RestAssured.port = 8080;
+//        RestAssured.basePath = "/kwetter/";
     }
 
     @Before
@@ -33,21 +29,9 @@ public class AppTest {
     @After
     public void tearDown() throws Exception {
     }
- @Test
-     public void makeSureThatGoogleIsUp() {
-         given().when().get("http://www.google.com").then().statusCode(200);
-     }
-//    @Test
-//    public void newAccountsTest() {
-//
-//        for (TestAccount t : generator.generateTestAccounts("group1")) {
-//            given()
-//                    .contentType("application/json")
-//                    .body(t)
-//                    .when()
-//                    .post("/accounts/")
-//                    .then()
-//                    .statusCode(200);
-//        }
-//    }
+
+    @Test
+    public void newAccountsTest() {
+        given().when().get("http://www.google.com").then().statusCode(200);
+    }
 }
