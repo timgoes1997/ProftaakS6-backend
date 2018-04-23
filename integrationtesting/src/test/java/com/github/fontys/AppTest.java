8a90d9489b36f1b0a5dc0a0370911904a742c5ae
Edwin van Rooij <edwinvrooij@gmail.com>
2018-04-23 11:49:43 +0200

    completed UNTESTED integration test for getRegisteredVehicles -- can't test because the functionality is still in another branch

--- integrationtesting/src/test/java/com/github/fontys/AppTest.java
+++ integrationtesting/src/test/java/com/github/fontys/AppTest.java
@@ -111,4 +111,53 @@ public class AppTest {
             fail(String.format("Could not retrieve all expected fields from the http response.\nError: %s", e.getMessage()));
         }
     }
+
+    @Test
+    public void getRegisteredVehiclesTest() {
+        // Get all registered vehicles
+        String body = given().get("vehicles/registered").asString();
+
+        // Parse body into JsonArray -- an array of vehicles
+        JsonArray array = parser.parse(body).getAsJsonArray();
+
+        // Check if we have at least one vehicle
+        if (array.size() == 0) {
+            fail("Size of array is 0. Did not get any vehicles from endpoint.");
+        }
+
+        // Get first vehicle, we'll validate the response using this object
+        JsonObject vehicle = array.get(0).getAsJsonObject();
+
+        // Try to get the expected fields
+        try {
+            vehicle.get("id").getAsInt();
+            vehicle.get("licensePlate").getAsString();
+            vehicle.get("vehicle").getAsJsonObject();
+            vehicle.get("proofOfOwnership").getAsString();
+
+            JsonArray bills = vehicle.get("bills").getAsJsonArray();
+            JsonObject bill = bills.get(0).getAsJsonObject();
+            // region Check bills on registered vehicle
+            // Note: Licenseplate does not exist anymore
+            // bill.get("licenseplate").getAsString();
+            bill.get("startDate").getAsJsonObject();
+            bill.get("endDate").getAsJsonObject();
+            bill.get("paid").getAsInt();
+            bill.get("price").getAsInt();
+            bill.get("status").getAsInt();
+
+            JsonArray trips = bill.get("trips").getAsJsonArray();
+            JsonObject trip = trips.get(0).getAsJsonObject();
+            // region Trips
+            trip.get("region").getAsString();
+            trip.get("price").getAsInt();
+            trip.get("miles").getAsInt();
+            trip.get("date").getAsJsonObject();
+            trip.get("time_rate").getAsInt();
+            // endregion
+            // endregion
+        } catch (Exception e) {
+            fail(String.format("Could not retrieve all expected fields from the http response.\nError: %s", e.getMessage()));
+        }
+    }
 }
