57057d358cc643f6713f21acbb7265be7fe6dc58
Tim Goes <Tim_Goes4@hotmail.nl>
2018-06-14 22:26:38 +0200

    Integrated queue-library as sub project.

--- Rekeningrijden/src/main/com/github/fontys/international/RouteTransformerGermany.java
+++ Rekeningrijden/src/main/com/github/fontys/international/RouteTransformerGermany.java
@@ -1,11 +1,11 @@
 package com.github.fontys.international;
 
+import com.github.fontys.entities.payment.Rate;
 import com.github.fontys.trackingsystem.services.beans.GenerationServiceImpl;
-import com.nonexistentcompany.RouteTransformer;
-import com.nonexistentcompany.domain.EULocation;
-import com.nonexistentcompany.domain.Rate;
-import com.nonexistentcompany.domain.RichRoute;
-import com.nonexistentcompany.domain.Route;
+import com.nonexistentcompany.lib.RouteTransformer;
+import com.nonexistentcompany.lib.domain.EULocation;
+import com.nonexistentcompany.lib.domain.ForeignRoute;
+import com.nonexistentcompany.lib.domain.RichRoute;
 
 import javax.inject.Inject;
 import java.util.ArrayList;
@@ -17,22 +17,25 @@ public class RouteTransformerGermany extends RouteTransformer {
     private GenerationServiceImpl generationService;
 
     @Override
-    public RichRoute generateRichRoute(Route route) {
+    public RichRoute generateRichRoute(ForeignRoute route, String ownCountry) {
         List<Rate> rates = new ArrayList<>();
-        List<EULocation> locations = route.getLocationList();
-        int distance = (int) generationService.calculateDistance(locations);
+//        List<EULocation> locations = route.getLocationList();
+//        int distance = (int) generationService.calculateDistance(locations);
+//
+//        // Rate in cents per kilometer
+//        // TODO: Calculate price per region, create Rate objects, calculate totalprice
+//        int rate = 20;
+//        Rate rateObject = new Rate(0, rate, distance);
+//        rates.add(rateObject);
+//
+//        // Price in cents
+//        int price = (int) generationService.generatePriceWithSingleRate(distance, rate);
+//
+//        // Vats in percentages
+//        int vats = 21;
+//        return new RichRoute(route.getOriginCountry(), route.getDrivenInCountry(), distance, price, vats, route.getLicensePlate(), "DE", rates)
+//
 
-        // Rate in cents per kilometer
-        // TODO: Calculate price per region, create Rate objects, calculate totalprice
-        int rate = 20;
-        Rate rateObject = new Rate(0, rate, distance);
-        rates.add(rateObject);
-
-        // Price in cents
-        int price = (int) generationService.generatePriceWithSingleRate(distance, rate);
-
-        // Vats in percentages
-        int vats = 21;
-        return new RichRoute(route.getOriginCountry(), route.getDrivenInCountry(), distance, price, vats, route.getLicensePlate(), "DE", rates);
+        return null;
     }
 }
